b47a5610ad5f5236fb2172249ca3ff36a1155ccc
SandIce <37013075+feizao67@users.noreply.github.com>
2021-11-15 15:50:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import util
 import requests
 from yiban import YiBan
@@ -10,17 +9,22 @@ def push(bark, err):
 
 
 def check(account, password, bark, address):
-    yb = YiBan(account, password, address)
-    yb.login()
-    yb.getHome()
-    if yb.submit() == True:
-        print("[%s]" % util.get_time(), yb.result_str)
-        push(bark, yb.result_str)
-    else:
-        print("[%s]" % util.get_time(), "%s 打卡成功" % yb.name)
+    try:
+        yb = YiBan(account, password, address)
+        yb.login()
+        yb.getHome()
+        if yb.submit() == True:
+            print(util.get_time(), yb.result_str)
+            push(bark, yb.result_str)
+        else:
+            print(util.get_time(), "%s 打卡成功" % yb.name)
+    except Exception as errors:
+        err = "%s %s" % (yb.account, errors)
+        print(util.get_time(), err)
+        push(bark, err)
 
 
-def data_input():
+def main():
     i = 2
     while i <= util.get_rows():
         if util.get_data(i, 3) is not None:
@@ -33,22 +37,11 @@ def data_input():
         else:
             if util.get_data(i, 4) is not None:
                 check(util.get_data(i, 1), util.get_data(i, 2),
-                      "这里填你的BARK", util.get_data(i, 4))
+                      "这里替换你的BARK", util.get_data(i, 4))
             else:
                 check(util.get_data(i, 1), util.get_data(i, 2),
-                      "这里填你的BARK", "广东省广州市天河区迎福路靠近广东金融学院")
-        time.sleep(1)
+                      "这里替换你的BARK", "广东省广州市天河区迎福路靠近广东金融学院")
         i = i+1
-
-
-def main():
-    try:
-        data_input()
-    except BaseException as errors:
-        err = "%s %s" % (yb.account, errors)
-        print("[%s]" % util.get_time(), err)
-        push(bark, err)
-        main()
 
 
 if __name__ == '__main__':
