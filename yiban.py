8a4c42ddf2b39f1de9a979f90db67cd30a0f4857
SandIce <37013075+feizao67@users.noreply.github.com>
2021-12-23 01:57:21 +0800

    简化

--- yiban.py
+++ yiban.py
@@ -1,40 +1,24 @@
+import time
 import base64
+import datetime
+import openpyxl
 import requests
-from util import get_today
 from Crypto.Cipher import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 
 
 class YiBan:
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/5.0.5",
-    }
-
     def __init__(self, account, passwd, address):
         self.account = account
         self.passwd = passwd
         self.address = address
         self.session = requests.session()
         self.name = ""
-        self.studentID = ""
-        self.url = ""
-        self.result = {}
-        self.result_str = ""
-
-    def request(self, url, method="get", headers=HEADERS, params=None, allow_redirects=True):
-        if method == "get":
-            req = self.session.get(
-                url=url, params=params, timeout=10, headers=headers, allow_redirects=allow_redirects)
-        else:
-            req = self.session.post(
-                url=url, data=params, timeout=10, headers=headers, allow_redirects=allow_redirects)
-        try:
-            return req.json()
-        except:
-            return None
+        self.HEADERS = {
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/5.0.6"
+        }
 
     def encryptPassword(self, pwd):
-        # 密码加密
         PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
             MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
             Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
@@ -56,7 +40,7 @@ class YiBan:
 
     def login(self):
         url = "https://mobile.yiban.cn/api/v4/passport/login"
-        param = {
+        data = {
             "device": "APPLE",
             "v": "5.0.2",
             "mobile": int(self.account),
@@ -70,17 +54,17 @@ class YiBan:
             "authCode": "",
             "sig": "934932a8993b5e23"
         }
-        header = {
+        headers = {
             'Origin': 'https://mobile.yiban.cn',
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/5.0.2',
             'Referer': 'https://mobile.yiban.cn',
             'AppVersion': '5.0.2'
         }
-        response = self.request(
-            url=url, method="post", params=param, headers=header)
+        response = self.session.post(
+            url=url, headers=headers, data=data).json()
         if response['response'] == 100:
             self.access_token = response['data']['access_token']
-            # print(self.access_token)
+            self.HEADERS["loginToken"] = self.access_token
             return response
         else:
             raise Exception("账号或密码错误")
@@ -89,8 +73,8 @@ class YiBan:
         params = {
             "access_token": self.access_token,
         }
-        r = self.request(
-            url="https://mobile.yiban.cn/api/v4/home", params=params)
+        r = self.session.get(
+            url="https://mobile.yiban.cn/api/v4/home", params=params).json()
         self.name = r["data"]["user"]["userName"]
         for i in r["data"]["hotApps"]:
             if i["name"] == "易广金":
@@ -99,54 +83,111 @@ class YiBan:
 
     def oauth(self):
         headers = {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/5.0.5",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 yiban_iOS/5.0.6",
             "Cookie": "loginToken={}; yibanM_user_token={}".format(self.access_token, self.access_token),
             "loginToken": self.access_token
         }
-
         data = {
             "client_id": "0b77c3ac53bd5c65",
             "redirect_uri": self.url,
         }
-
-        oauth = self.request(
-            url="https://oauth.yiban.cn/code/usersure", method="post", params=data, headers=headers)
-
+        oauth = self.session.post(
+            url="https://oauth.yiban.cn/code/usersure", headers=headers, data=data).json()
         return oauth["reUrl"]
 
     def submit(self):
-        self.HEADERS["loginToken"] = self.access_token
-
         auth_1 = self.session.get(
             url=self.oauth(), headers=self.HEADERS, allow_redirects=False)
-
         auth_2 = self.session.get(
             url=auth_1.headers["Location"], headers=self.HEADERS, allow_redirects=False)
-
         home = self.session.get(
             url=auth_2.headers["Location"], headers=self.HEADERS, allow_redirects=False)
-
-        self.studentID = home.headers["Location"].split("studentID=")[1]
-
-        bind = self.request(
+        studentID = home.headers["Location"].split("studentID=")[1]
+        self.session.get(
             url="https://ygj.gduf.edu.cn/Handler/device.ashx?flag=checkBindDevice", headers=self.HEADERS)
-
-        params = {
+        data = {
             "flag": "save",
-            "studentID": "{0}".format(self.studentID),
+            "studentID": "{0}".format(studentID),
             "date": "{0}".format(get_today()),
             "health": "体温37.3℃以下（正常）",
             "address": "{0}".format(self.address),
             "isTouch": "否",
             "isPatient": "不是"
         }
-
-        self.result = self.request(
-            url="https://ygj.gduf.edu.cn/Handler/health.ashx?", method="post", headers=self.HEADERS, params=params)
-
-        if int(self.result["code"]) != 0:
-            self.result_str = "{0} 打卡失败，失败原因：{1}".format(
-                self.name, self.result["msg"])
+        result = self.session.post(
+            url="https://ygj.gduf.edu.cn/Handler/health.ashx?", headers=self.HEADERS, data=data).json()
+        if int(result["code"]) == 0:
             return True
         else:
-            return False
+            return result["msg"]
+
+
+def get_time():
+    return time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime())
+
+
+def get_today():
+    return time.strftime("%Y-%m-%d", time.localtime())
+
+
+def get_rows():
+    rows = openpyxl.load_workbook('./data.xlsx').worksheets[0].max_row
+    return rows
+
+
+def get_data(x, y):
+    cell = openpyxl.load_workbook('./data.xlsx').worksheets[0].cell(x, y).value
+    return cell
+
+
+def push(bark, err):
+    api = "https://api.day.app/%s/易班打卡异常提醒/%s?" % (bark, err)
+    send = requests.post(api)
+
+
+def check(account, password, bark, address):
+    t = 1
+    while t <= 3:
+        try:
+            yb = YiBan(account, password, address)
+            yb.login()
+            yb.getHome()
+            submit = yb.submit()
+            if submit is True:
+                print(get_time(), "[第%s次执行] %s 打卡成功" % (t, yb.name))
+                t = 5
+            else:
+                print(get_time(), "{0} 打卡失败，失败原因：{1}".format(yb.name, submit))
+                push(bark, "{0} 打卡失败，失败原因：{1}".format(yb.name, submit))
+                t = 5
+        except Exception as errors:
+            if t <= 2:
+                err = "[第%s次执行] %s %s" % (t, yb.account, errors)
+                print(get_time(), err)
+                push(bark, err)
+                t += 1
+            else:
+                err = "[第%s次执行] %s %s" % (t, yb.account, errors)
+                print(get_time(), err)
+                push(bark, err)
+                t = 5
+
+
+if __name__ == '__main__':
+    i = 2
+    while i <= get_rows():
+        if get_data(i, 3) is not None:
+            if get_data(i, 4) is not None:
+                check(get_data(i, 1), get_data(i, 2),
+                      get_data(i, 3), get_data(i, 4))
+            else:
+                check(get_data(i, 1), get_data(i, 2),
+                      get_data(i, 3), "广东省广州市天河区迎福路靠近广东金融学院")
+        else:
+            if get_data(i, 4) is not None:
+                check(get_data(i, 1), get_data(i, 2),
+                      "这里替换主（帮别人打卡的管理员的）BARK", get_data(i, 4))
+            else:
+                check(get_data(i, 1), get_data(i, 2),
+                      "这里替换主（帮别人打卡的管理员的）BARK", "广东省广州市天河区迎福路靠近广东金融学院")
+        i += 1
